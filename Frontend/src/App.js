eff99e6b546d3baf78f798ca205df994114ef0de
Joao Victor <joaobarbosaa053@gmail.com>
2024-04-11 14:33:51 -0300

    v

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,13 +2,13 @@ import TelaMenu from "./interfaces/TelaMenuSistema.js";
 import Tela404 from "./interfaces/Tela404.js"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TelaCadastroHospede from "./interfaces/TelaCadastroHospede.jsx";
-import TelaCadastroVagas from "./interfaces/TelaCadastroVagas.jsx";
 //import TelaCadastroPessoa from "./Formularios/FormPessoa.jsx";
 import FormTefone from "./Formularios/FormTelefone.jsx";
 
 import './App.scss';
 import 'boxicons/css/boxicons.min.css';
 import TelaCadastroTelefone from "./interfaces/TelaCadastroTelefone.jsx";
+import TelaCadastroVagas from "./interfaces/TelaCadastroVagas.jsx";
 function App() {
   return (
     <div className="App">
